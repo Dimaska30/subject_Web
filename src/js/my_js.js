07a004ed5e0d633d873e051ee3194d826de51fd1
Dima Pleskunov <pleskunov_dima@mail.ru>
2022-12-26 01:09:01 +0600

    Create article page with functionals

---
+++ src/js/my_js.js
@@ -0,0 +1,11 @@
+
+
+const choose = src => {
+    console.log('hi')
+    document.getElementsByClassName('article__img')[0].src = src
+}
+
+const zoom = src => {
+    //'<div id="magnify"><img src="'+i_path+'"><div id="close-popup"><i></i></div></div>'
+    
+}
